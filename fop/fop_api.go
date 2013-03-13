d73030674a9440a378fde5cb9f61fd7552e5b522
Cheney <me@chenye.org>
2013-03-13 13:24:28 +0800

    add fop

---
+++ fop/fop_api.go
@@ -0,0 +1,88 @@
+package fop
+
+import (
+	"net/http"
+	
+	. "github.com/qiniu/api/conf"
+	"github.com/qiniu/rpc"
+	"github.com/qiniu/api/rs"
+	"github.com/qiniu/api/encodeuri"
+	"github.com/qiniu/api/auth/digest"
+)
+
+// ----------------------------------------------------------
+
+type Client struct {
+	Conn rpc.Client
+	DownloadToken string
+}
+
+func New(downloadToken string) Client {
+	t := digest.NewTransport(ACCESS_KEY, SECRET_KEY, nil)
+	client := &http.Client{Transport: t}
+	return Client{rpc.Client{client}, downloadToken}
+}
+
+func NewEx(downloadToken string, t http.RoundTripper) Client {
+	client := &http.Client{Transport: t}
+	return Client{rpc.Client{client}, downloadToken}
+}
+
+// ----------------------------------------------------------
+
+type InfoImage struct {
+	Format string
+	Width int
+	Height int
+	ColorModel string
+}
+
+type ValType struct {
+	Val string
+	Type int
+}
+
+type ExifImage map[string] ValType
+
+func ImageInfo(l rpc.Logger, imageURL string) (info InfoImage, err error) {
+	err = rpc.DefaultClient.Call(l, &info, imageURL + "?imageInfo")
+	return
+}
+
+func ImageExif(l rpc.Logger, imageURL string) (exif ExifImage, err error) {
+	err = rpc.DefaultClient.Call(l, &exif, imageURL + "?exif")
+	return
+}
+
+type MogrifyOptions struct {
+	Mode int `uri:""`
+	Width int `uri:"w"`
+	Height int `uri:"h"`
+	Quality int `uri:"q"`
+	Format string `uri:"format"`
+}
+
+func ImageMogrifyForPreview(
+	imageURL string, mogrifyOptions MogrifyOptions) (previewURL string) {
+	opts, _ := encodeuri.Marshal(mogrifyOptions)
+	return imageURL + "?imageView" + opts
+}
+
+// -----------------------------------------------------------------------------
+
+type FileProfile struct {
+	Expires int
+	Url string
+	MimeType string
+	Hash string
+	Fsize int64
+}
+
+func (fop *Client) get(entryURI string) (url string, err error) {
+	profile := new(FileProfile)
+	err = fop.Conn.Call(nil, &profile, RS_HOST + "/get/" + rs.EncodeURI(entryURI))
+	if err == nil {
+		url = profile.Url
+	}
+	return
+}
---
+++ fop/fop_api_test.go
@@ -0,0 +1,57 @@
+package fop
+
+import (
+	"testing"
+	
+	. "github.com/qiniu/api/conf"
+	"github.com/qiniu/api/rs"
+)
+
+var bucketName = "a"
+var key = "cccd"
+var policy = rs.GetPolicy {
+	Scope: "a",
+	Expires: 3600,
+}
+var client Client
+
+func init() {
+	ACCESS_KEY = "tGf47MBl1LyT9uaNv-NZV4XZe7sKxOIa9RE2Lp8B"
+	SECRET_KEY = "zhbiA6gcQMEi22uZ8CBGvmbnD2sR8SO-5S8qlLCG"
+	client = New(policy.Token())
+}
+
+func TestImageInfo(t *testing.T) {
+	_, err := ImageInfo(nil, "http://cheneya.qiniudn.com/ffdfd_9")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestImageExif(t *testing.T) {
+	_, err := ImageExif(nil, "http://qiniuphotos.qiniudn.com/gogopher.jpg")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestImageMogrifyForPreview(t *testing.T) {
+	url := "http://qiniuphotos.qiniudn.com/gogopher.jpg"
+	opts := MogrifyOptions {
+		Mode: 2,
+		Height: 200,
+	}
+	u := ImageMogrifyForPreview(url, opts)
+	if url + "?imageView/2/h/200" != u {
+		t.Error(u)
+	}
+}
+
+func TestGet(t *testing.T) {
+	_, err := client.get("a:aa")
+	if err != nil {
+		t.Error(err)
+	}
+}
