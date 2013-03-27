1fa8f007b25b42b7d5f18d384f1525f6958038ac
Cheney <me@chenye.org>
2013-03-27 15:38:57 +0800

    remove auto delete

--- io/io_api_test.go
+++ io/io_api_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 	"bytes"
 	"os"
+	"io"
+	"crypto/rand"
+	"fmt"
 
 	"github.com/qiniu/api/rs"
 	. "github.com/qiniu/api/conf"
@@ -23,13 +26,21 @@ func init() {
 	SECRET_KEY = os.Getenv("QINIU_SECRET_KEY")
 }
 
+func randomBoundary() string {
+	var buf [30]byte
+	_, err := io.ReadFull(rand.Reader, buf[:])
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x", buf[:])
+}
+
 func TestPut(t *testing.T) {
-	key := "test_1"
+	key := "test_put_" + randomBoundary()
 	buf := bytes.NewBuffer(nil)
 	ret := new(interface{})
 
 	buf.WriteString("hello! new Put")
-	rs.New().Delete(nil, bucket, key)
 	err := Put(nil, ret, 
 		policy.Token(), bucket, key, buf, int64(buf.Len()), extra)
 	if err != nil {
@@ -40,9 +51,8 @@ func TestPut(t *testing.T) {
 func TestPutFile(t *testing.T) {
 	var ret interface{}
 	localFile := "./io_api_test.go"
-	key := "test_put_file"
+	key := "test_put_file_" + randomBoundary()
 
-	rs.New().Delete(nil, bucket, key)
 	err := PutFile(nil, &ret, policy.Token(), bucket, key, localFile, extra)
 	if err != nil {
 		t.Error(err)
