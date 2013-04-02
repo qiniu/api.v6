c16c8d8f35b55c69d1da832846ff7ce463def426
xushiwei <xushiweizh@gmail.com>
2013-04-02 21:16:44 +0800

    resumable io

--- io/io_api.go
+++ io/io_api.go
@@ -3,94 +3,92 @@ package io
 import (
 	"io"
 	"os"
-	"encoding/base64"
 	"mime/multipart"
-	
-	. "github.com/qiniu/api/conf"
+	"encoding/base64"
 	"github.com/qiniu/rpc"
+	. "github.com/qiniu/api/conf"
 )
 
 type PutExtra struct {
-    CallbackParams  string  // 当 uptoken 指定了 CallbackUrl，则 CallbackParams 必须非空
-    Bucket          string
-    CustomMeta      string  // 可选。用户自定义 Meta，不能超过 256 字节
-    MimeType        string  // 可选。在 uptoken 没有指定 DetectMime 时，用户客户端可自己指定 MimeType
+	CallbackParams  string  // 当 uptoken 指定了 CallbackUrl，则 CallbackParams 必须非空
+	Bucket          string
+	CustomMeta      string  // 可选。用户自定义 Meta，不能超过 256 字节
+	MimeType        string  // 可选。在 uptoken 没有指定 DetectMime 时，用户客户端可自己指定 MimeType
 }
 
 type PutRet struct {
-	// 如果 uptoken 没有指定 ReturnBody，那么返回值是标准的 PutRet 结构 
-    Hash string `json:"hash"`
+	Hash string `json:"hash"` // 如果 uptoken 没有指定 ReturnBody，那么返回值是标准的 PutRet 结构
 }
 
-func Put(l rpc.Logger, ret interface{},
-	uptoken, key string, data io.Reader, extra *PutExtra) error {
+func writeMultipart(writer *multipart.Writer, uptoken, key string, data io.Reader, extra *PutExtra) (err error) {
 
-	url := UP_HOST + "/upload"
+	// auth
+	err = writer.WriteField("auth", uptoken)
+	if err != nil {
+		return
+	}
+
+	// action
+	action := "/rs-put/" + encodeURI(extra.Bucket + ":" + key)
+	if extra.MimeType != "" {
+		action += "/mimeType/" + encodeURI(extra.MimeType)
+	}
+	if extra.CustomMeta != "" {
+		action += "/meta/" + encodeURI(extra.CustomMeta)
+	}
+	err = writer.WriteField("action", action)
+	if err != nil {
+		return
+	}
+
+	// params
+	if extra.CallbackParams != "" {
+		err = writer.WriteField("params", extra.CallbackParams)
+		if err != nil {
+			return
+		}
+	}
+
+	// file
+	writerBuf, err := writer.CreateFormFile("file", key)
+	if err != nil {
+		return
+	}
+	_, err = io.Copy(writerBuf, data)
+	return
+}
+
+func Put(l rpc.Logger, ret interface{}, uptoken, key string, data io.Reader, extra *PutExtra) error {
+
 	r, w := io.Pipe()
 	defer r.Close()
 	writer := multipart.NewWriter(w)
-	
+
 	go func() {
-		var err error = nil
-		defer func() {
-			writer.Close()
-			w.CloseWithError(err)
-		}()
-		
-		// auth
-		err = writer.WriteField("auth", uptoken)
-		if err != nil {
-			return
-		}
-		
-		// action
-		action := "/rs-put/" + encodeURI(extra.Bucket + ":" + key)
-		if extra.MimeType != "" {
-			action += "/mimeType/" + encodeURI(extra.MimeType)
-		}
-		if extra.CustomMeta != "" {
-			action += "/meta/" + encodeURI(extra.CustomMeta)
-		}
-		err = writer.WriteField("action", action)
-		if err != nil {
-			return
-		}
-	
-		// params
-		if extra.CallbackParams != "" {
-			err = writer.WriteField("params", extra.CallbackParams)
-			if err != nil {
-				return
-			}
-		}
-	
-		// file
-		writer_buf, err := writer.CreateFormFile("file", key)
-		if err != nil {
-			return
-		}
-		_, err = io.Copy(writer_buf, data)
+		err := writeMultipart(writer, uptoken, key, data, extra)
+		writer.Close()
+		w.CloseWithError(err)
 	}()
-	
+
 	contentType := writer.FormDataContentType()
-	return rpc.DefaultClient.CallWith64(l, ret, url, contentType, r, 0)
+	return rpc.DefaultClient.CallWith64(l, ret, UP_HOST + "/upload", contentType, r, 0)
 }
 
-func PutFile(l rpc.Logger, ret interface{},
-	uptoken, key string, localFile string, extra *PutExtra) (err error) {
+func PutFile(l rpc.Logger, ret interface{}, uptoken, key string, localFile string, extra *PutExtra) (err error) {
 
 	f, err := os.Open(localFile)
 	if err != nil {
 		return
 	}
 	defer f.Close()
-	
+
 	return Put(l, ret, uptoken, key, f, extra)
 }
 
 // ----------------------------------------------------------
 
 func GetUrl(domain string, key string, dntoken string) (downloadUrl string) {
+
 	url := domain + "/" + key
 	if dntoken == "" {
 		return url
@@ -103,3 +101,6 @@ func GetUrl(domain string, key string, dntoken string) (downloadUrl string) {
 func encodeURI(uri string) string {
 	return base64.URLEncoding.EncodeToString([]byte(uri))
 }
+
+// ----------------------------------------------------------
+
