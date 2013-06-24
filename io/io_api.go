292cb9f77c925b99e38a0d0fbab2bfb234b3d106
wangtuanjie <wangtuanjiezh@gmail.com>
2013-06-24 10:02:47 +0800

    update gosdk/io to v6

--- io/io_api.go
+++ io/io_api.go
@@ -4,77 +4,68 @@ import (
 	"io"
 	"os"
 	"mime/multipart"
-	"encoding/base64"
+	"fmt"
+	"hash/crc32"
+	"net/textproto"
+	"strconv"
+	"strings"
+
 	"github.com/qiniu/rpc"
 	. "github.com/qiniu/api/conf"
 )
 
+// ----------------------------------------------------------
+
+const UNDEFINED_KEY = "?"
+
+// ----------------------------------------------------------
 type PutExtra struct {
-	CallbackParams  string  // 当 uptoken 指定了 CallbackUrl，则 CallbackParams 必须非空
-	Bucket          string
-	CustomMeta      string  // 可选。用户自定义 Meta，不能超过 256 字节
-	MimeType        string  // 可选。在 uptoken 没有指定 DetectMime 时，用户客户端可自己指定 MimeType
+	Params   map[string]string    //可选，用户自定义参数，必须以 "x:" 开头
+	                              //若不以x:开头，则忽略
+	MimeType string               //可选，当为 "" 时候，服务端自动判断 
+	Crc32    uint32
+	CheckCrc uint32
+	        // CheckCrc == 0: 表示不进行 crc32 校验
+	        // CheckCrc == 1: 对于 Put 等同于 CheckCrc = 2；对于 PutFile 会自动计算 crc32 值
+	        // CheckCrc == 2: 表示进行 crc32 校验，且 crc32 值就是上面的 Crc32 变量
 }
 
 type PutRet struct {
-	Hash string `json:"hash"` // 如果 uptoken 没有指定 ReturnBody，那么返回值是标准的 PutRet 结构
+	Hash string `json:"hash"`  // 如果 uptoken 没有指定 ReturnBody，那么返回值是标准的 PutRet 结构
+	Key  string `json:"key"`   //如果传入的 key == UNDEFINED_KEY，则服务端返回 key
 }
 
-func writeMultipart(writer *multipart.Writer, uptoken, key string, data io.Reader, extra *PutExtra) (err error) {
+// ----------------------------------------------------------
 
-	// auth
-	err = writer.WriteField("auth", uptoken)
-	if err != nil {
-		return
-	}
+func Put(ret interface{}, uptoken, key string, data io.Reader, extra *PutExtra) error {
 
-	// action
-	action := "/rs-put/" + encodeURI(extra.Bucket + ":" + key)
-	if extra.MimeType != "" {
-		action += "/mimeType/" + encodeURI(extra.MimeType)
-	}
-	if extra.CustomMeta != "" {
-		action += "/meta/" + encodeURI(extra.CustomMeta)
-	}
-	err = writer.WriteField("action", action)
-	if err != nil {
-		return
-	}
-
-	// params
-	if extra.CallbackParams != "" {
-		err = writer.WriteField("params", extra.CallbackParams)
-		if err != nil {
-			return
+	// CheckCrc == 1: 对于 Put 等同于 CheckCrc == 2
+	var extra1 PutExtra
+	if extra != nil {
+		extra1 = *extra
+		if extra1.CheckCrc == 1 {
+			extra1.CheckCrc = 2
 		}
 	}
-
-	// file
-	writerBuf, err := writer.CreateFormFile("file", key)
-	if err != nil {
-		return
-	}
-	_, err = io.Copy(writerBuf, data)
-	return
+	return putEx(ret, uptoken, key, key, data, &extra1)
 }
 
-func Put(l rpc.Logger, ret interface{}, uptoken, key string, data io.Reader, extra *PutExtra) error {
-
+func putEx(ret interface{}, uptoken, key, localFile string, data io.Reader, extra *PutExtra) error {
 	r, w := io.Pipe()
 	defer r.Close()
 	writer := multipart.NewWriter(w)
 
 	go func() {
-		err := writeMultipart(writer, uptoken, key, data, extra)
+		err := writeMultipart(writer, uptoken, key, localFile, data, extra)
 		writer.Close()
 		w.CloseWithError(err)
 	}()
 
 	contentType := writer.FormDataContentType()
-	return rpc.DefaultClient.CallWith64(l, ret, UP_HOST + "/upload", contentType, r, 0)
+	return rpc.DefaultClient.CallWith64(nil, ret, UP_HOST, contentType, r, 0)
 }
 
-func PutFile(l rpc.Logger, ret interface{}, uptoken, key string, localFile string, extra *PutExtra) (err error) {
+func PutFile(ret interface{}, uptoken, key string, localFile string, extra *PutExtra) (err error) {
 
 	f, err := os.Open(localFile)
 	if err != nil {
@@ -82,14 +73,92 @@ func PutFile(l rpc.Logger, ret interface{}, uptoken, key string, localFile strin
 	}
 	defer f.Close()
 
-	return Put(l, ret, uptoken, key, f, extra)
+	return putEx(ret, uptoken, key, localFile, f, extra)
+}
+
+
+
+/*
+ * extra.CheckCrc:
+ *      0:     不进行crc32校验
+ *      1:     以writeMultipart自动生成crc32的值，进行校验
+ *      2:     以extra.Crc32的值，进行校验
+ *      other: 和0一样，不进行crc32校验   
+ */
+func writeMultipart(writer *multipart.Writer, uptoken, key, localFile string, data io.Reader, extra *PutExtra) (err error) {
+
+	if extra == nil {
+		extra = &PutExtra{}
+	}
+
+	//token
+	if err = writer.WriteField("token", uptoken); err != nil {
+		return
+	}
+
+	//key
+	if key != UNDEFINED_KEY {
+		if err = writer.WriteField("key", key); err != nil {
+			return
+		}
+	}
+
+	// extra.Params
+	if extra.Params != nil {
+		for k, v := range extra.Params {
+			if strings.HasPrefix(k, "x:") {
+				err = writer.WriteField(k, v)
+				if err != nil {
+					return 
+				}
+			}
+		}
+	}
+
+	//extra.CheckCrc 
+	h := crc32.NewIEEE()
+	data1 := data
+	var crc32 int64
+	if extra.CheckCrc == 1 {
+		data1 = io.TeeReader(data, h)
+	}
+
+	//file 
+	head := make(textproto.MIMEHeader)
+	if localFile == "" {
+		localFile = "renameit"
+	}
+	head.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
+			escapeQuotes("file"), escapeQuotes(localFile)))
+	if  extra.MimeType != "" {
+		head.Set("Content-Type", extra.MimeType)
+	}
+
+	writerBuf, err := writer.CreatePart(head)
+	if err != nil {
+		return
+	}
+	_, err = io.Copy(writerBuf, data1)
+
+	//extra.CheckCrc 
+	if extra.CheckCrc == 1 {
+		crc32 = int64(h.Sum32())
+		err = writer.WriteField("crc32", strconv.FormatInt(crc32, 10))
+	} else if extra.CheckCrc == 2 {
+		crc32 = int64(extra.Crc32)
+		err = writer.WriteField("crc32", strconv.FormatInt(crc32, 10))
+	}
+	//extra.CheckCrc ==0 || extra.CheckCrc == other 
+	//do not CheckCrc so not write crc32 filed
+	return 
 }
 
 // ----------------------------------------------------------
 
-func encodeURI(uri string) string {
-	return base64.URLEncoding.EncodeToString([]byte(uri))
-}
+var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
 
-// ----------------------------------------------------------
+func escapeQuotes(s string) string {
+	return quoteEscaper.Replace(s)
+}
 
--- io/io_api_test.go
+++ io/io_api_test.go
@@ -2,11 +2,12 @@ package io
 
 import (
 	"bytes"
-	"os"
-	"io"
-	"fmt"
-	"testing"
 	"crypto/rand"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"os"
+	"testing"
 	"github.com/qiniu/api/rs"
 	. "github.com/qiniu/api/conf"
 )
@@ -15,18 +16,32 @@ var bucket = "a"
 var policy = rs.PutPolicy {
 	Scope: bucket,
 }
-var extra = &PutExtra {
-	MimeType: "text/plain",
-	Bucket: bucket,
-	CallbackParams: "hello=yes",
+var upString = "hello qiniu world"
+var extra =  []*PutExtra {
+	&PutExtra {
+		MimeType: "text/plain",
+		CheckCrc: 1,
+	},
+	&PutExtra {
+		MimeType: "text/plain",
+		CheckCrc: 0,
+	},
+	nil,
 }
 
 func init() {
 	ACCESS_KEY = os.Getenv("QINIU_ACCESS_KEY")
 	SECRET_KEY = os.Getenv("QINIU_SECRET_KEY")
+
+	for _, v := range extra {
+		if v != nil {
+			v.Crc32 = crc32String(upString)
+		}
+	}
 }
 
 func randomBoundary() string {
+
 	var buf [30]byte
 	_, err := io.ReadFull(rand.Reader, buf[:])
 	if err != nil {
@@ -35,20 +50,28 @@ func randomBoundary() string {
 	return fmt.Sprintf("%x", buf[:])
 }
 
+func crc32String(s string) uint32 {
+	h := crc32.NewIEEE()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
+
 func TestPut(t *testing.T) {
-	key := "test_put_" + randomBoundary()
+
 	buf := bytes.NewBuffer(nil)
 	ret := new(PutRet)
-
-	buf.WriteString("hello! new Put")
-	err := Put(nil, ret, 
-		policy.Token(nil), key, buf, extra)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if (ret.Hash != "FsqT8gw5b4TDw_eD5UTXip9VMCQy") {
-		t.Error("wrong hash")
+	for i, v := range extra {
+		key := "test_put_" + randomBoundary()
+		//test undefined key example
+		if i == 1 {
+			key = "?"
+		}
+		buf.WriteString(upString)
+		err := Put(ret, policy.Token(nil), key, buf, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(ret.Hash, ret.Key)
 	}
 }
 
