abd7f89af281c7fd3076d6b1e407df7760ab1ebf
wangtuanjie <wangtuanjiezh@gmail.com>
2013-06-29 22:04:59 +0800

    EOF support

--- rsf/rsf_api.go
+++ rsf/rsf_api.go
@@ -1,10 +1,10 @@
 package rsf
 
 import (
-	"errors"
 	"net/http"
 	"net/url"
 	"strconv"
+	"io"
 	"github.com/qiniu/rpc"
 	"github.com/qiniu/api/auth/digest"
 	. "github.com/qiniu/api/conf"
@@ -44,23 +44,24 @@ func NewEx(t http.RoundTripper) Client {
 }
 
 // ----------------------------------------------------------
-
+// 1. 首次请求 marker = ""
+// 2. 无论 err 值如何，均应该先看 entries 是否有内容
+// 3. 如果后续没有更多数据，err 返回 EOF，markerOut 返回 ""（但不通过该特征来判断是否结束）
 func (rsf Client) ListPrefix(l rpc.Logger, bucket, prefix, marker string, limit int) (entries []ListItem, markerOut string, err error) {
-
-	if bucket == "" {
-		err = errors.New("bucket could not be nil")
-		return 
-	}
-
 	URL := makeListURL(bucket, prefix, marker, limit)
 	listRet := ListRet{}
 	err = rsf.Conn.Call(l, &listRet, URL)
+	if len(listRet.Items) == 0 || listRet.Marker == "" {
+		return listRet.Items, "", io.EOF
+	}
 	return listRet.Items, listRet.Marker, err
 }
 
 func makeListURL(bucket, prefix, marker string, limit int) string {
 	query := make(url.Values)
-	query.Add("bucket", bucket)
+	if bucket != "" {
+		query.Add("bucket", bucket)
+	}
 	if prefix != "" {
 		query.Add("prefix", prefix)
 	}
--- rsf/rsf_api_test.go
+++ rsf/rsf_api_test.go
@@ -3,11 +3,13 @@ package rsf
 import (
 	"os"
 	"testing"
+	"io"
 	. "github.com/qiniu/api/conf"
 )
 
 var bucketName = "a"
 var client Client
+var maxNum = 100000
 
 func init() {
 	ACCESS_KEY = os.Getenv("QINIU_ACCESS_KEY")
@@ -24,13 +26,27 @@ func TestList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(ret) != 5 {
-		t.Fatal("ret num err")
+	if len(ret) != 5 && err != io.EOF {
+		t.Fatal("TestList failed:", "expect len(ret) 5, but:", len(ret))
 	}
-	ret, _, err = client.ListPrefix(nil, bucketName, "", marker, 100)
-	if err != nil {
-		t.Fatal(err)
+
+	ret, _, err = client.ListPrefix(nil, bucketName, "", marker, 10000)
+	if err != nil && err != io.EOF {
+		t.Fatal("TestList failed:", "marker failed:", err)
 	}
+
 }
 
+func TestEof(t *testing.T) {
+	_, _, err := client.ListPrefix(nil, bucketName, "", "", maxNum)
 
+	if err != io.EOF {
+		t.Fatal("TestEof failed:", "expect EOF but:", err)
+	}
+
+	_, _, err = client.ListPrefix(nil, bucketName, "", "", -1)
+
+	if err != io.EOF {
+		t.Fatal("TestEof failed:", "expect EOF but:", err)
+	}
+}
