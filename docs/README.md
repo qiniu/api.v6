7354b682bcb4d159e64c741be740f59cf2ef079e
wangtuanjie <wangtuanjiezh@gmail.com>
2013-06-30 12:10:28 +0800

    fix unit test

--- docs/README.md
+++ docs/README.md
@@ -569,15 +569,23 @@ import "github.com/qiniu/api/rsf"
 func main() {
 	ACCESS_KEY = "<YOUR_APP_ACCESS_KEY>"
 	SECRET_KEY = "<YOUR_APP_SECRET_KEY>"
-	// allItems 存储所有文件列表信息
-	var allItems []rsf.ListItem
+	var entries []rsf.ListItem
+	var marker = ""
+	var err error
+	var limit = 1000
+	var prefix = "<prefix>"
+	
 	for err != io.EOF {
 		entries, marker, err = rsf.New(nil).ListPrefix(logger, bucketName,
 			prefix, marker, limit)
 		if err != nil && err != io.EOF {
 			break
 		}
-		allItems = append(allItems, entries...)
+	
+		for _, v := range entries {
+			//这里为了演示，依次输出文件列表
+			fmt.Println(v)
+		}
 	}
 }
 ```
--- docs/gist/rsf.go
+++ docs/gist/rsf.go
@@ -4,25 +4,30 @@ package gist
 import "github.com/qiniu/api/rsf"
 // @endgist
 
-import "io"
-
-var limit = 1000
-var prefix = "<prefix>"
-var entries []rsf.ListItem
-var marker = ""
-var err error
+import (
+	"io"
+	"fmt"
+)
 
 func listPrefixDemo() {
 	// @gist listPrefix
-	// allItems 存储所有文件列表信息
-	var allItems []rsf.ListItem
+	var entries []rsf.ListItem
+	var marker = ""
+	var err error
+	var limit = 1000
+	var prefix = "<prefix>"
+
 	for err != io.EOF {
 		entries, marker, err = rsf.New(nil).ListPrefix(logger, bucketName,
 			prefix, marker, limit)
 		if err != nil && err != io.EOF {
 			break
 		}
-		allItems = append(allItems, entries...)
+
+		for _, v := range entries {
+			//这里为了演示，依次输出文件列表
+			fmt.Println(v)
+		}
 	}
 	// @endgist
 }
