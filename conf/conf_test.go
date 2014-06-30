73a846bca6719701f55f9231463d2023ad3926c0
longbai <slongbai@gmail.com>
2014-06-30 17:38:36 +0800

    add ua test

---
+++ conf/conf_test.go
@@ -0,0 +1,27 @@
+package conf
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestUA(t *testing.T) {
+	err := SetUser("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = SetUser("错误的UA")
+	if err == nil {
+		t.Fatal("expect an invalid ua format")
+	}
+}
+
+func TestFormat(t *testing.T) {
+	v := formatUserAgent("test")
+	if strings.Contains(v, "test") {
+		t.Fatal("should include user")
+	}
+	if strings.HasPrefix(v, "QiniuGo/"+version) {
+		t.Fatal("invalid format")
+	}
+}
