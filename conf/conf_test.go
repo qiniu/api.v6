a79e8c44d8c87b84eb701be723e2840e17f684a8
longbai <slongbai@gmail.com>
2014-06-30 21:48:47 +0800

    fixed user regexp

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -8,17 +8,22 @@ import (
 func TestUA(t *testing.T) {
 	err := SetUser("")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("expect no error")
 	}
 	err = SetUser("错误的UA")
 	if err == nil {
 		t.Fatal("expect an invalid ua format")
 	}
+	err = SetUser("Test0-_.")
+	if err != nil {
+		t.Fatal("expect no error")
+	}
 }
 
 func TestFormat(t *testing.T) {
-	v := formatUserAgent("test")
-	if !strings.Contains(v, "test") {
+	str := "tesT0.-_"
+	v := formatUserAgent(str)
+	if !strings.Contains(v, str) {
 		t.Fatal("should include user")
 	}
 	if !strings.HasPrefix(v, "QiniuGo/"+version) {
